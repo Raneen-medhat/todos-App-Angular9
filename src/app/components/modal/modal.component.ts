621b1822451485b09cfe14bc6143733f149a2fe5
Raneen <raneen.medhat@gmail.com>
2020-02-28 22:00:42 +0200

    todos-edit todo item

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, AfterViewInit, Output, EventEmitter  } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
 
-  constructor() { }
+export class ModalComponent implements AfterViewInit {
+  closeResult: string;
+  @Input() todo;
+  @ViewChild("content") content: ElementRef;
+  @Output() save = new EventEmitter();
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  openVerticallyCentered() {
+    this.modalService.open(this.content, { centered: true });
+  }
+
+  saveEdit = (value, id) => {
+    this.save.emit({value, id});
+  }
+
+  ngAfterViewInit(): void {
+    this.openVerticallyCentered()
   }
 
 }
